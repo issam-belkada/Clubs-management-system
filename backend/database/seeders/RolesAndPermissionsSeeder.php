54ce2b92655c3ee3a0cad9559d7b72d8389cb41f
Braxhim19 <benzedirabrahim3@gmail.com>
2025-12-12 04:37:32 +0100

    fix:dbseed

--- backend/database/seeders/RolesAndPermissionsSeeder.php
+++ backend/database/seeders/RolesAndPermissionsSeeder.php
@@ -102,7 +102,7 @@ class RolesAndPermissionsSeeder extends Seeder
             "view attendance",
 
         ]);
-        $club_memeber ->givePermissionTo([
+        $club_member->givePermissionTo([
             'create posts',
             'create tasks',
             'view posts',
