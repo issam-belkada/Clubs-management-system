8602a4d8cdfe3bb483c3ccb52e27a6c86bd3fd02
Braxhim19 <benzedirabrahim3@gmail.com>
2025-12-12 02:14:50 +0100

    fix:model fillable fields

--- backend/database/migrations/2025_12_11_174108_create_events_table.php
+++ backend/database/migrations/2025_12_11_174108_create_events_table.php
@@ -23,7 +23,7 @@ return new class extends Migration
             $table->foreignId('event_type_id')->constrained('event_types')->onDelete('cascade');
             $table->foreignId('club_id')->constrained('clubs')->onDelete('cascade');
             $table->foreignId('created_by')->constrained('users')->onDelete('cascade');
-            $table->text('custom_form')->nullable();
+            $table->json('custom_form')->nullable();
             $table->string('event_image')->nullable();
             $table->timestamps();
         });
